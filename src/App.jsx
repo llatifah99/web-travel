86d5b3e331a144cbfef20ca83cf8d190a2665cc6
llatifah99 <litalatifah6@gmail.com>
2025-05-17 09:51:36 +0700

    make page howtoaccess

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Menu, X, HandHeart, Zap } from "lucide-react";
 import WhyChooseUs from "./page/WhyChooseUs";
-import CardHowToAccess from "./components/CardHowToAccess";
+import HowToAccess from "./page/HowToAccess";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -102,7 +102,7 @@ function App() {
 
       {/* Konten tambahan */}
       <WhyChooseUs />
-      <CardHowToAccess />
+      <HowToAccess />
     </>
   );
 }
---
+++ src/page/HowToAccess.jsx
@@ -0,0 +1,14 @@
+import CardHowToAccess from "../components/CardHowToAccess";
+
+const HowToAccess = () => {
+  return (
+    <>
+      <div>
+        <h1>How to Access Our Services</h1>
+        <CardHowToAccess></CardHowToAccess>
+      </div>
+    </>
+  );
+};
+
+export default HowToAccess;
