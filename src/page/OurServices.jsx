2d43132e835a4f75dc37f3900378b435b09b0bb5
llatifah99 <litalatifah6@gmail.com>
2025-05-18 13:03:26 +0700

    edit our services contents

--- src/page/OurServices.jsx
+++ src/page/OurServices.jsx
@@ -5,25 +5,24 @@ const OurServices = () => {
       id: 1,
       imageLink:
         "https://images.unsplash.com/photo-1619467416348-6a782839e95f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      text: "Travel",
+      text: "Travel Itinerary",
     },
     {
       id: 2,
       imageLink:
         "https://images.unsplash.com/photo-1618773928121-c32242e63f39?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      text: "Hotel",
+      text: "Hotel Booking",
     },
     {
       id: 3,
       imageLink:
-        "https://images.unsplash.com/photo-1619467416348-6a782839e95f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      text: "Plane",
+        "https://images.unsplash.com/photo-1620146596504-95527348e0ac?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Flight Booking",
     },
     {
       id: 4,
-      imageLink:
-        "https://images.unsplash.com/photo-1619467416348-6a782839e95f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      text: "Travel",
+      imageLink: "https://www.aca.co.id/cmsprd/uploads/C_1864%201710398777.jpg",
+      text: "Travel Insurance",
     },
   ];
   return (
