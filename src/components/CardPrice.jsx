7aae90f70eb54784aa9e57f1e9e75d0cb0d940b5
llatifah99 <litalatifah6@gmail.com>
2025-05-19 11:26:42 +0700

    styling

--- src/components/CardPrice.jsx
+++ src/components/CardPrice.jsx
@@ -48,7 +48,7 @@ const CardPrice = ({ priceList }) => {
             className="bg-blue-001541 text-yellow-FFB729 w-[280px] h-[340px] flex flex-col items-center px-6 py-12 rounded-xl"
           >
             <h1 className="text-2xl mb-2">{item.title}</h1>
-            <h2 className="text-5xl mb-2 font-bold">${item.price}</h2>
+            <h2 className="text-5xl mb-4 font-bold">${item.price}</h2>
             <ul className="text-md list-disc list-inside">
               {item.features.split(".").map((poin, index) => (
                 <li key={index}>{poin}</li>
